e6ae33878a2507c2882420764ec2f26523ef2136
laharke <ronconimauro@yahoo.com.ar>
2021-09-04 21:47:14 -0300

    Added the basic HTML and some CSS to make the calculator, plus a couple of functions

---
+++ script.js
@@ -0,0 +1,22 @@
+//functions to operate on numbers
+function add (a, b) {
+    return a + b;
+};
+
+function subtract (a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a * b;
+};
+
+function divide (a, b){
+    return a / b;
+};
+
+//operate function
+function operate (operator, num1, num2) {
+    return operator(num1, num2);
+};
+
